0ae76c23698b590cd0ef308ba956f22890d95bcb
Gytis Trikleris <gytis@redhat.com>
2018-02-06 18:25:47 +0100

    Name service

---
+++ spring-boot-istio-tls-name/src/test/java/io/openshift/booster/BoosterApplicationTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openshift.booster;
+
+import com.jayway.restassured.RestAssured;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoosterApplicationTest {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = String.format("http://localhost:%s/api", port);
+    }
+
+    @Test
+    public void shouldGetName() {
+        when().get("name")
+                .then()
+                .assertThat()
+                .statusCode(is(equalTo(200)))
+                .and()
+                .body(is(equalTo("World")));
+    }
+
+
+}
